27b51e89c513dfe517eb3d0c62113f401fbbae83
Evil Dark Angel <evildarkangel666@gmail.com>
2013-05-23 09:27:58 +0200

    Errors

--- remote.php
+++ remote.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set("display_errors", 1);
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+
 $startTime = explode(' ', microtime());
 
 echo "\n [-] Joomla Vulnerability Scanner (Remote) [-]\n\n";
